9f56850f999c7698eb616050d05e0fa21ed79f32
Rahul Rai <therahulray@gmail.com>
2025-09-01 10:45:04 +0530

    fix: resolved merge conflict in current-affairs page

--- src/app/admin/current-affairs/[id]/page.tsx
+++ src/app/admin/current-affairs/[id]/page.tsx
@@ -24,14 +24,12 @@ export default function AddCurrentAffairsPage() {
   const [active, setActive] = useState(true);
 
   // 📌 Category & Sub Category
-<<<<<<< HEAD
-  const [categories, setCategories] = useState([]); 
-=======
   const [categories, setCategories] = useState([]);
->>>>>>> 069d2caf20ecd77a386506a11c4c6b25730a9e0b
   const [subCategories, setSubCategories] = useState([]);
   const [category, setCategory] = useState("");
   const [subCategory, setSubCategory] = useState("");
+
+
 
   // 📌 Media
   const [imageFile, setImageFile] = useState<File | null>(null);
